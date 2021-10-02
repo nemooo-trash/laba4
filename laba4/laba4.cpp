b21ec12e305c8440b3f8e62020cefc136989d10a
Антон Богушевич <1066964@edu.rut-miit.ru>
2021-10-02 03:37:24 +0300

    Добавьте файлы проекта.

---
+++ laba4/laba4.cpp
@@ -0,0 +1,33 @@
+﻿
+
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+
+
+class Math {
+public:
+	double sin_x, cos_x;
+public:
+	Math(double x) {
+		sin_x = sin(x);
+		cos_x = cos(x);
+	}
+	double get_cos() { return 1 / cos_x; };
+	double get_cosec() { return 1 / sin_x; };
+	double get_ctg() { return cos_x / sin_x; };
+};
+
+int main()
+{
+	setlocale(LC_ALL, "Russian");
+	float g;
+	cout << "Введите переменную: ";
+	cin >> g;
+	Math Start(g);
+	double sec = Start.get_cos();
+	double csec = Start.get_cosec();
+	double ctg = Start.get_ctg();
+	cout << "Секанс = " << sec << ", Косеканс = " << csec << ", Котангенс = " << ctg;
+}
